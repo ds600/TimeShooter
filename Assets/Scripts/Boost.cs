9686d910acdbded7455f71c8ccbf67def2b1a9b9
ds600 <ageofdavid1@outlook.com>
2021-05-06 10:13:22 +0200

    Making Sure

---
+++ Assets/Scripts/Boost.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boost : MonoBehaviour
+{
+    public float boostX, boostY, boostZ;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(boostX, boostY, boostZ);
+    }
+}
---
+++ Assets/Scripts/Player/Dummy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+    float gravity = -29.81f;
+    public float jumpHeight = 3f;
+    Vector3 velocity;
+    bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -8f;
+        }
+        
+
+        velocity.y = Mathf.Sqrt(jumpHeight * -8f * gravity);
+        controller.Move(velocity * Time.deltaTime);
+        
+        Invoke("DownToEarth", 1);
+        Invoke("Jump", 3);
+    }
+
+    void DownToEarth()
+    {
+        velocity.y = gravity;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
